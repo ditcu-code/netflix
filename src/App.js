3c13d1dbdf5ee792ee7398b83aec8b36d502128c
pradaayu <pradaayu@gmail.com>
2020-06-10 09:32:36 +0700

    [Ani] combine header and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
+import Header from './layout/Header';
+import Footer from './layout/Footer'
 // import Routes from "./routes/Routes"
 
 function App() {
   return (
     <div>
+      <Header/>
       <HomePage/>
+       <Footer/>
       ssdasss
       {/* <Routes/> */}
     </div>
